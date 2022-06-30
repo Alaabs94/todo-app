35c026ce14385231bf986fb9cc5aed2feca89f23
unknown <alaabs.94@gmail.com>
2022-06-30 03:17:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoInputs from "./component/TodoInputs";
+import TodoList from "./component/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App m-5">
+      <h4>TODO LIST with Redux</h4><br/><br/>
+      <TodoInputs />
+      <TodoList />
     </div>
   );
 }
---
+++ src/component/TodoItem.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo, updateTodo, filtre } from "../JS/actions/actions";
+import { useState } from "react";
+
+function TodoItem({ todo }) {
+  const dispatch = useDispatch();
+  const [editable, setEditable] = useState(false);
+
+  const [newt, setNewt] = useState(todo.name);
+  const [clsbtn, setClsbtn] = useState("btn btn-secondary m-2");
+  const isDone = useSelector((state) => state.todos.isDone);
+
+  return (
+    <div>
+      <div
+        className="row mx-2 align-item-center">
+        <div
+          className="col">
+          {editable ? (
+            <input
+              type="texte"
+              classeName="form-contol"
+              value={newt}
+              onChange={(e) => setNewt(e.target.value)}
+            />
+          ) : (
+            <h4>{todo.name}</h4>
+          )}
+        </div>
+        <button
+          className={clsbtn}
+          onClick={() => {
+            dispatch(filtre(todo.id, todo.isDone));
+            if (todo.isDone === false) {
+              setClsbtn("btn btn-success m-2");
+            } else if (todo.isDone === true) {
+              setClsbtn("btn btn-secondary m-2");
+            }
+          }}
+        >
+          {todo.isDone ? "Done" : "unDone"}
+        </button>
+        <button
+          className="btn btn-primary m-2"
+          onClick={() => {
+            setEditable(!editable);
+            if (editable) {
+              dispatch(updateTodo(todo.id, newt));
+            }
+          }}
+        >
+          {editable ? "Update" : "Edit"}
+        </button>
+        <button
+          onClick={() => dispatch(deleteTodo(todo.id))}
+          className="btn btn-danger m-2"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
